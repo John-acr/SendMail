5cb0deed81421d13dbed3ded9572b63291bf87de
John-acr <79087680+John-acr@users.noreply.github.com>
2021-03-06 16:42:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function sendMail(params){
         to_name: document.getElementById("toName").value,
         message: document.getElementById("msg").value,
     };
-    emailjs.send('gmail', 'template_k0gdq18',tempParams)
+    emailjs.send('service_fsl2oq6','template_k0gdq18',tempParams)
     .then(function(res){
         console.log("success", res.status);
     })
